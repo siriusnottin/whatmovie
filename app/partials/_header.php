afb167bcc3a1a55f58c425cc4bf4c117a41170c7
Sirius Nottin <30043962+siriusnottin@users.noreply.github.com>
2025-04-22 17:04:19 +0200

    feat: refactor header structure and styles for improved layout

--- app/partials/_header.php
+++ app/partials/_header.php
@@ -1,20 +1,20 @@
-  <header>
-    <div class="breandcrumbs">
-      <i class="ci-House_01"></i>
-      <div>
-        <a href="/">Home</a>
-        <span class="separator">/</span>
-        <a href="/signin">Signin</a>
-      </div>
+<header>
+  <div class="breandcrumbs">
+    <i class="ci-House_01"></i>
+    <div>
+      <a href="/">Home</a>
+      <span class="separator">/</span>
+      <a href="/signin">Signin</a>
     </div>
-    <form action="search.php" method="GET" class="search-form">
-      <i class="ci-Search_Magnifying_Glass"></i>
-      <input type="text" name="query" placeholder="Search a movie"
-        class="search-input" aria-label="Search for a movie">
-      <span class="shortcut-txt">⌘K</span>
-    </form>
-    <nav class="account-nav">
-      <a href="/account">Sirius</a>
-      <i class="ci-User_Circle"></i>
-    </nav>
-  </header>
+  </div>
+  <form action="/search" method="GET" class="search-form">
+    <i class="ci-Search_Magnifying_Glass"></i>
+    <input type="text" name="query" placeholder="Search a movie" class="search-input" aria-label="Search for a movie">
+    <span class="shortcut-txt">⌘K</span>
+  </form>
+  <nav class="account-nav">
+    <a href="/signin">Signin</a>
+    <i class="ci-Log_Out"></i>
+    <!-- <i class="ci-User_Circle"></i> -->
+  </nav>
+</header>
