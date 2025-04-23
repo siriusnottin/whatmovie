7ab2b7c2668598563b91416d89c37272d6b870fa
Sirius Nottin <30043962+siriusnottin@users.noreply.github.com>
2025-04-23 20:45:00 +0200

    feat: add route for account profile display

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Dotenv\Dotenv;
 use App\Core\Router;
 use App\Controllers\AuthController;
+use App\Controllers\AccountController;
 
 // Load environment variables
 $dotenv = Dotenv::createImmutable(__DIR__);
@@ -16,6 +17,7 @@ $authController = new AuthController();
 $router->addRoute('POST', '/signin', [$authController, 'signin']);
 $router->addRoute('POST', '/signup', [$authController, 'signup']);
 $router->addRoute('POST', '/signout', [$authController, 'signout']);
+$router->addRoute('GET', '/account', [new AccountController(), 'showProfile']);
 
 // Dispatch the request
 $router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
